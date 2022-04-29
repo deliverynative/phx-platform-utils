a514b9f95afc15e32651c2a540859d197f5be6bf
tomcon <tconchie@gmail.com>
2022-04-28 18:46:19 -0600

    #patch: Expand faker to auto-create more data in factories

--- priv/templates/phx.gen.dn.context/factory.ex
+++ priv/templates/phx.gen.dn.context/factory.ex
@@ -7,7 +7,11 @@ defmodule <%= inspect context.module %>.Factory do
   def generate() do
     %Model{
       # Don't forget to implement the rest of this map for fake data!
-      id: Faker.UUID.v4()
+      id: Faker.UUID.v4()<%= if length(schema.faker_attrs) > 0 do %>,<% end %>
+      <%= for {col, mod, func} <- schema.faker_attrs do %>
+      <%= col %>: <%= inspect mod %>.<%= func %>(),<% end %>
+      <%= for {col, mod, func, args} <- schema.faker_attrs do %>
+      <%= col %>: <%= inspect mod %>.<%= func %>(<%= Enum.join(args, ", ") %>),<% end %>
     }
   end
 end
