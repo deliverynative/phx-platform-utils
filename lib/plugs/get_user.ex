d21e7c3614e21a5ee678c766966c5a98ba732199
tomcon <tconchie@gmail.com>
2022-05-20 12:14:33 -0600

    #minor: Add a plug to get user from decoded jwt

---
+++ lib/plugs/get_user.ex
@@ -0,0 +1,31 @@
+defmodule PhxPlatformUtils.Plugs.GetUser do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user =
+      conn
+      |> get_token!()
+      |> decode_token!()
+
+    conn
+    |> put_session(:user, user)
+  end
+
+  defp get_token!(conn) do
+    case get_req_header(conn, "authorization") do
+      ["Bearer " <> token] -> token
+      _ -> nil
+    end
+  end
+
+  defp decode_token!(nil) do
+    nil
+  end
+
+  defp decode_token!(token) do
+    [_, payload, _] = token |> String.split(".")
+    payload |> Base.decode64!(padding: false) |> Jason.decode!(keys: :atoms)
+  end
+end
