14ee23a576014eb1674dbe9546775f1703b484f3
tomcon <tconchie@gmail.com>
2022-06-09 16:30:36 -0600

    #minor: Update Factory generation to allow more complex generated types

--- priv/templates/phx.gen.dn.context/factory.ex
+++ priv/templates/phx.gen.dn.context/factory.ex
@@ -9,10 +9,7 @@ defmodule <%= inspect context.module %>.Factory do
     %Model{
       # Don't forget to implement the rest of this map for fake data!
       id: Faker.UUID.v4()<%= if length(schema.faker_attrs) > 0 do %>,<% end %>
-      <%= for {col, mod, func} <- schema.faker_attrs do %>
-      <%= col %>: <%= inspect mod %>.<%= func %>(),<% end %>
-      <%= for {col, mod, func, args} <- schema.faker_attrs do %>
-      <%= col %>: <%= inspect mod %>.<%= func %>(<%= Enum.join(args, ", ") %>),<% end %>
+      <%= for {col, def} <- schema.faker_attrs do %><%= col %>: <%= inspect def %>,<% end %>
     }
   end
 
