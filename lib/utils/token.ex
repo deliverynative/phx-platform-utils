20d89a57df0efd0a8b55c1568e6c7828940d09aa
tomcon <tconchie@gmail.com>
2022-07-21 23:57:41 -0600

    #patch: 403 on missing user user object on request, instead of 500

--- lib/utils/token.ex
+++ lib/utils/token.ex
@@ -2,13 +2,18 @@ defmodule PhxPlatformUtils.Utils.Token do
   alias PhxPlatformUtils.Errors
 
   def claims(conn) do
-    conn.assigns[:user]
+    conn.assigns[:user] || %{}
   end
 
   def get_matching_permissions(conn, allowed_permissions) do
-    allowed = MapSet.new(allowed_permissions)
-    users_perms = MapSet.new(claims(conn).permissions)
-    MapSet.intersection(allowed, users_perms) |> MapSet.to_list()
+    allowed = allowed_permissions |> MapSet.new()
+
+    conn
+    |> claims()
+    |> Map.get(:permissions, [])
+    |> MapSet.new()
+    |> MapSet.intersection(allowed)
+    |> MapSet.to_list()
   end
 
   def has_permission!(conn, allowed_permissions) do
